34b018f6f5c91c00156e9e6488c704f6e66aeb2a
Yeah <yyndzmzl@gmail.com>
2024-06-22 15:14:09 +0800

    Update mooc_monkey_meta.js

--- js/mooc_monkey_meta.js
+++ js/mooc_monkey_meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         yeah_MOOC脚本
 // @namespace    https://res.yeah666.com
-// @version      2.1.1
+// @version      2.1.3
 // @description  慕课脚本，可以显示答案（题库），刷文档，做测试
 // @author       yeah
 // @icon         https://res.yeah666.com/img/logocore.png
@@ -18,5 +18,6 @@
 // @match        https://www.icourse163.org/spoc/learn/*
 // @match        https://www.icourse163.org/mooc/*
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @require      https://lib.baomitu.com/jqueryui/1.13.2/jquery-ui.js
 // @run-at       document-end
 // ==UserScript==
