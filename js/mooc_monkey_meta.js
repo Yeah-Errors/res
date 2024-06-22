5ff5d8fdc75e08432178eb31ba32ac35c7376242
Yeah <yyndzmzl@gmail.com>
2024-06-22 16:59:58 +0800

    更新 mooc_monkey_meta.js

--- js/mooc_monkey_meta.js
+++ js/mooc_monkey_meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         yeah_MOOC脚本
+// @name         Yeah_MOOC脚本
 // @namespace    https://res.yeah666.com
 // @version      2.1.7
-// @description  慕课脚本，可以显示答案（题库），刷文档，做测试
-// @author       yeah
+// @description  慕课脚本，可以显示答案（题库），刷文档（暂不支持），做测试（暂不支持）
+// @author       Yeah
 // @icon         https://res.yeah666.com/img/logocore.png
 // @grant        unsafeWindow
 // @grant        GM_xmlhttpRequest
