9670fc58916e4c3d56eaeb263f9299e6f3c3b5fb
Yeah <yyndzmzl@gmail.com>
2024-06-22 15:04:40 +0800

    Create mooc_monkey_meta.js

---
+++ js/mooc_monkey_meta.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         yeah_MOOC脚本
+// @namespace    https://res.yeah666.com
+// @version      2.1.1
+// @description  慕课脚本，可以显示答案（题库），刷文档，做测试
+// @author       yeah
+// @icon         https://res.yeah666.com/img/logocore.png
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @grant        GM.xmlHttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @license      MIT
+// @connect      *
+// @match        https://www.icourse163.org/learn/*
+// @match        http://www.icourse163.org/learn/*
+// @match        http://www.icourse163.org/spoc/learn/*
+// @match        https://www.icourse163.org/spoc/learn/*
+// @match        https://www.icourse163.org/mooc/*
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @run-at       document-end
+// ==UserScript==
